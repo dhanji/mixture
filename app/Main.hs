c533a85e5fbb2b03ff8b1fde7d2576ba32213bac
Dhanji R. Prasanna <dhanji@gmail.com>
2019-05-02 19:18:39 -0700

    Command line flag for mix file

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,12 @@
 module Main where
 
+import System.Environment
 import Text.RawString.QQ
+
 import Mix
 
+
+banner :: String
 banner = [r|
                   .__          __
             _____ |__|__  ____/  |_ __ _________   ____
@@ -13,7 +17,17 @@ banner = [r|
 
           a MIX computer in λaskell |]
 
+
 main :: IO ()
 main = do
     putStrLn banner
-    executeMix "a.mix"
+    args <- getArgs
+
+    case parse args of
+      Left msg    -> putStrLn msg
+      Right file  -> executeMix file
+
+
+parse :: [String] -> Either String String
+parse [arg] = Right arg
+parse _     = Left "\nusage: mixture <file.mix>"
