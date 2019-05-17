ce774b9cbfd9ce89d7f12f0510b5fbb24b340118
Dhanji Prasanna <dhanji@squareup.com>
2019-05-17 15:40:46 -0700

    Added test suite and some quickcheck properties

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Mix.Data.Cell
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain suite
+
+suite :: TestTree
+suite = testGroup "\nMix Test Suite" [properties]
+
+properties :: TestTree
+properties = testGroup "QuickCheck: Data.Cell" [
+    testProperty "Roundtrip: Int -> toCell -> toInt" $
+      \n -> (toInt . toCell) n == (n :: Int)
+
+  , testProperty "addCells result equals integer sum" $
+      \(n, m) -> toInt (addCells (toCell n) (toCell m)) == (n :: Int) + (m :: Int)
+  ]
